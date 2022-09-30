8173caf72684b3ae68629b485baefa65cd823739
ReflextionsDev <reflextionsdev@gmail.com>
2022-09-30 14:01:59 -0400

    update app, README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-.env needs API_KEY
+# Pexels Microservice
+Node/Express wrapper for the Pexels API
 
-# Environment Vars
-API_KEY
+## Scripts
+- npm run start (defaults to local env)
+- npm run dev
+- npm run prod
+
+## Environment Vars
+- API_KEY (Pexels API key https://www.pexels.com/api/new/)
+- PORT (optional, defaults to 3000)
+
+## Dependencies
+    "dotenv": "^16.0.2",
+    "express": "^4.18.1",
+    "node-fetch": "^3.2.10",
+    "pexels": "^1.4.0"
+
+## Dev Dependencies
+- cross-env (script env var support for powershell)
--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-// add URL var as env // also type dev or not (run dev > localhost)
-// env local vs. prod
-// if local use host, if prod use domain from env
-
+// Environment
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
+const env = process.env.NODE_ENV || 'local'
+const port = process.env.PORT || 3000
+console.log('Environment: ', env)
 
+// Express
 import express from 'express'
-import { apiRouter } from './api/routes.js';
-
-
 const app = express();
 
-
-console.log('environment: ', process.env.NODE_ENV)
-
 // Routes
+import { apiRouter } from './api/routes.js';
 app.use('/pexels', apiRouter)
 
-
-const port = process.env.PORT || 3000
-
-
+// App
 app.listen(port, () => {
-    console.log(`Listening to port http://localhost:${port}`);
+    console.log(`Listening to port ${port}`);
 });
